d9750ff583671e074b75e24d312a02e6b3779906
Student <m.d.oroark@gmail.com>
2016-01-27 12:08:36 -0700

    first and last commit - loops all solved

--- main.js
+++ main.js
@@ -1,148 +1,54 @@
-// FUNCTION ONE
+// GLOBAL ARRAY
 
-getName = function(object){
-	return object.name;
+var students = [{
+ name: 'Liz',
+ age: 25,
+ city: 'Boulder'
+},{
+ name: 'Meghan',
+ age: 27,
+ city: 'Denver'
+},{
+ name: 'Trent',
+ age: 32,
+ city: 'Boulder'
+},{
+ name: 'Chelsea',
+ age: 24,
+ city: 'Boulder'
+},{
+ name: 'Amir',
+ age: 18,
+ city: 'Denver'
+}];
+
+// LOOP ONE
+
+for(var i=0; i<students.length; i++) {
+ console.log(students[i].age);
 }
 
-console.log(getName({ name: 'Luisa', age: 25 }))
+// LOOP TWO
 
-//FUNCTION TWO
-
-totalLetters = function(array){
-	var total = 0;
-	for (i = 0; i < array.length; i++) {
-		total += array[i].length
-		
-	}
-	return total;
+for(var i=0; i<students.length; i++) {
+ console.log(students[i].name + ',' + students[i].city);
 }
 
-totalLetters(['hello','my','name','is','max'])
+// LOOP THREE
 
-totalLetters(['javascript', 'is', 'awesome'])
-
-totalLetters(['what', 'happened', 'to', 'my', 'function'])
-
-// FUNCTION THREE
-
-keyValue = function (key, value) {
-	var object = {}
-	object[key] = value;
-	return object;
-
-}
-
-// FUNCTION FOUR
-
-negativeIndex = function (array, negNum){
-    var reversal = array.length + negNum;
-    return array[reversal];
-    
-}
-
-// FUNCTION FIVE
-
-removeM = function(string){
-        var newArray = []    
-	    var newStrArr = string.split('') 
-	    for(i = 0; i< newStrArr.length; i++){
-	   
-	        if (newStrArr[i] != 'm'){
-	            newArray.push(newStrArr[i]);
-	            
-	        }
-	    }
-	    
-	   return newArray.join('');
-		}
-	
-removeM('vermouth')
-
-// FUNCTION SIX
-
-var superman = {
-    gender: 'male',
-    strength: 'infinite',
-    phoneNum: 'none of yo bizzness'
-}
-
-var objPrinter = function(object){
-   for (var key in object) {
-       console.log(key + ' is ' + object[key]);
-   }
-    
-}
-objPrinter(superman)
-
-
-// FUNCTION SEVEN
-
-var vowels = function(string){
-    var letters = string.split('');
-    var answer = []
-    for (var i = 0; i < letters.length; i++){
-        if (letters[i] === 'a' || letters[i] == 'e' || letters[i] == 'i' || letters[i] == 'o' || letters[i] == 'u' ){
-            answer.push(letters[i]);
-        }
+for(var i=0; i<students.length; i++) {
+    if (students[i].city === 'Boulder'){
+        console.log(students[i].name + ' is from ' + students[i].city);
     }
-    
-    return answer
-}    
+}
 
-vowels('the fox jumped over the lazy dog');
+// LOOP FOUR
 
-vowels('alabama');
-
-//FUNCTION EIGHT
-
-var twins = function (array) {
-    for (var i = 0; i < array.length; i += 2){
-        if (array[i]  !== array[i +1]){
-            return false;
-        }
+for(var i=0; i<students.length; i++) {
+    if (students[i].age > 25){
+        console.log(students[i].name + ' is older than ' + 25);
     }
-    return true;
 }
-
-twins([1,1,2,2,3,3,4,4,5,5])
-
-twins(['a', 'a', 'b', 'c', 'd', 'd'])
-
-twins(['a', 'a', 'b', 'b', 'c', 'c'])
-
-
-//FUNCTION NINE
-
-var ifTrue = function(bool){
-        if (bool === true){
-            return true;
-        }
-        }
-
-var or = function(boolArr){
-    boolArr.forEach(ifTrue)
-    return false;
-}
-
-or([false, false, true, false])
-
-//FUNCTION TEN
-
-unique = function(array){
-    array.sort();
-    uniqueArr = []
-    for(i = 0; i < array.length; i++){
-      if (array[i]  !== array[i +1]){
-            uniqueArr.push(array[i]);
-        }
-    }
-    return uniqueArr
-}
-
-unique(['todd', 'avery', 'maria', 'avery'])
-
-
-
 
 
 
